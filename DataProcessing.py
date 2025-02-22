e3dd411a350c74849f4879d60dcbd47824c8f55e
JonathE0 <165324472+JonathE0@users.noreply.github.com>
2025-02-22 14:08:43 -0700

    Update DataProcessing.py

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 # Define the path to your text file
 file_path = "data1.txt"  # Replace with the actual path to your file
 
@@ -19,9 +20,9 @@ with open(file_path, "r") as file:
             # Extract the values at positions 11, 12, and 13 (latitude, longitude, altitude)
             latitude = float(parts[11])
             longitude = float(parts[12])
-            altitude = float(parts[13])
+            #altitude = float(parts[13])
             # Append the values as a list to the 2D list
-            posa_values.append([latitude, longitude, altitude])
+            posa_values.append([latitude, longitude])
     
 
 # Print the extracted POSA values
@@ -30,15 +31,4 @@ for values in posa_values:
     List_2D_posa.append(values)
 
 np.array(List_2D_posa)
-
-
-lattitude = [row[0] for row in List_2D_posa]
-longitude = [row[1] for row in List_2D_posa]
-
-
-# Print the result
-print("Lattitude:", lattitude)
-print("=" * 50)  # Prints 50 dashes
-print("Lattitude:", longitude)
-
-
+print(List_2D_posa)
